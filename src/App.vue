b570341f2adf4bf39b098cf33623a16f60ac435d
龙骑将杨影枫 <catwindboy@hotmail.com>
2020-10-20 17:39:31 +0800

    分片

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,12 @@
             files: [uploadFile]
           }
         } = file;
-        console.log(this.slice);
-        console.log(this.slice(uploadFile));
+        const size = uploadFile.size;
+        console.log(size %9);
+        console.log(this.slice(uploadFile, (size > 500*1024*1024) ? (size /10): 1024 * 1024 * 50));
               
       },
-      slice(file, piece = 1024 * 1024 * 5) {
+      slice(file, piece = 1024 * 1024 * 50) {
       let totalSize = file.size; // 文件总大小
       let start = 0; // 每次上传的开始字节
       let end = start + piece; // 每次上传的结尾字节
@@ -32,7 +33,6 @@
         // File对象继承自Blob对象，因此包含slice方法
         let blob = file.slice(start, end); 
         chunks.push(blob)
-
         start = end;
         end = start + piece;
       }
